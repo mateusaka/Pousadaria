f4d6e4953be5969bab9f6d72e7340b9589327753
Mateus Correia <correia.mateus@hotmail.com>
2024-05-29 11:05:36 -0300

    Adicionado o primeiro teste(incompleto) para registrar uma nova promoção

---
+++ spec/system/inn_owner/inn_owner_register_promo_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'Dono de pousada registra uma nova promoção' do
+  it 'a partir da tela inicial' do
+    inn_owner = InnOwner.create!(first_name: 'Joao', last_name: 'Almeida',
+                                  document: '53783222001', email: 'joao@email.com', password: '123456')
+    inn_owner.create_inn!(name: 'Pousada do Almeidinha', registration_number: '30638898000199', description: 'Um bom lugar',
+                          address_attributes: { address: 'Rua X', number: '100', city: 'Manaus', state: 'AM', postal_code: '69067-080', neighborhood: 'Centro'})
+    inn.inn_rooms.create!(name: 'Quarto com Varanda', size: 35, guest_limit: 3)
+    inn.inn_rooms.create!(name: 'Quarto Térreo', size: 30, guest_limit: 3)
+    login_as inn_owner, scope: :inn_owner
+
+    visit root_path
+    click_on 'Gestão de Pousadas'
+    click_on 'Área de promoções'
+    click_on 'Adicionar nova promoção'
+
+    expect(page).to have_content 'Nome da promoção'
+    expect(page).to have_content 'Data inicial'
+    expect(page).to have_content 'Data final'
+    expect(page).to have_content 'Nome da promoção'
+
+    espero ver os campos
+    nome promo
+    data inicial
+    data final
+    selec quarto
+    % de desc
+  end
+end
